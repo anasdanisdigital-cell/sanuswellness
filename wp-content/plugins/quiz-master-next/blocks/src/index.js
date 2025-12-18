130d1cd1e8c6ea25f20f63983c238c36f33d4c4e
anasdanisdigital-cell <anas.danisdigital@gmail.com>
2025-12-18 09:49:45 +0500

    Updated/Added simply-static-1-1766032406

---
+++ wp-content/plugins/quiz-master-next/blocks/src/index.js
@@ -0,0 +1,1 @@
+import{registerBlockType}from'@wordpress/blocks';import'./style.scss';import Edit from'./edit';import metadata from'./block.json';import{qsmBlockIcon}from'./component/icon';import{createHigherOrderComponent}from'@wordpress/compose';const save=(props)=>null;registerBlockType(metadata.name,{icon:qsmBlockIcon,edit:Edit,save:save,});
