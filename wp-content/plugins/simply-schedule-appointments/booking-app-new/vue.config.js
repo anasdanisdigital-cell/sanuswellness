5ba402af52faa962dcf14c95b52db253bcefbfa4
anasdanisdigital-cell <anas.danisdigital@gmail.com>
2025-12-18 09:51:39 +0500

    Updated/Added simply-static-1-1766032406

---
+++ wp-content/plugins/simply-schedule-appointments/booking-app-new/vue.config.js
@@ -0,0 +1,2 @@
+module.exports={filenameHashing:false,transpileDependencies:['vue-tel-input'],chainWebpack:config=>{config.plugin('html').tap(args=>{args[0].title='Book an appointment'
+return args})},configureWebpack:{output:{jsonpFunction:'jsonpFunction'}},devServer:{proxy:{'/wp-':{target:'http://ssa.localdev',changeOrigin:true},'/getApi.php':{target:'http://ssa.localdev/wp-content/plugins/simply-schedule-appointments',changeOrigin:true}}},assetsDir:'static'}
